32d492876d7f9cb06025fb84923928c872e16c5f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-11-25 00:24:07 +0100

    start work on tutorial addon

---
+++ iscore_addon_tutorial.cpp
@@ -0,0 +1,119 @@
+#include "iscore_addon_tutorial.hpp"
+
+
+iscore_addon_tutorial::iscore_addon_tutorial()
+{
+
+}
+
+iscore_addon_tutorial::~iscore_addon_tutorial()
+{
+
+}
+
+/**
+ * @brief iscore_addon_tutorial::version
+ * @return The version of the plug-in
+ */
+iscore::Version iscore_addon_tutorial::version() const
+{
+    return {};
+}
+
+/**
+ * @brief iscore_addon_tutorial::key
+ * @return A unique identifier for the plug-in
+ */
+UuidKey<iscore::Plugin> iscore_addon_tutorial::key() const
+{
+    // Can be generated with "uuidgen" on linux or OS X.
+    return_uuid("e8601d50-e29e-4437-8c65-fcee42655a0b");
+}
+
+/**
+ * @brief iscore_addon_tutorial::required
+ * @return Features that this plug-in requires.
+ */
+QStringList iscore_addon_tutorial::required() const
+{
+    return {};
+}
+
+/**
+ * @brief iscore_addon_tutorial::offered
+ * @return Features that this plug-in provides.
+ */
+QStringList iscore_addon_tutorial::offered() const
+{
+    return {};
+}
+
+/**
+ * @brief iscore_addon_tutorial::updateSaveFile
+ * This function is called if someone tries to load
+ * a save file that was created with a previous version
+ * of this plug-in.
+ * This allows to update the JSON data in order to load
+ * it in the current version.
+ */
+void iscore_addon_tutorial::updateSaveFile(
+        QJsonObject& obj,
+        iscore::Version obj_version,
+        iscore::Version current_version)
+{
+
+}
+
+/**
+ * @brief iscore_addon_tutorial::factoryFamilies
+ * This function allows the plug-in to provide new factory types.
+ * All plug-ins will be scanned for factories of the provided type.
+ *
+ * For instance, a new interface to display some elements in a toolbar.
+ */
+std::vector<std::unique_ptr<iscore::FactoryListInterface> >
+iscore_addon_tutorial::factoryFamilies()
+{
+    return {};
+}
+
+
+/**
+ * @brief iscore_addon_tutorial::factories
+ *
+ * This function allows the plug-in to provide implementations for the
+ * factory types provided earlier.
+ *
+ * For instance, facotires for the elements that will go in the toolbar.
+ */
+std::vector<std::unique_ptr<iscore::FactoryInterfaceBase> >
+iscore_addon_tutorial::factories(
+        const iscore::ApplicationContext& ctx,
+        const iscore::AbstractFactoryKey& factoryName) const
+{
+    return {};
+}
+
+/**
+ * @brief iscore_addon_tutorial::make_applicationPlugin
+ *
+ * This function allows to instantiate an application-wide object.
+ * These objects are instantiated before any factory.
+ */
+iscore::GUIApplicationContextPlugin*
+iscore_addon_tutorial::make_applicationPlugin(
+        const iscore::GUIApplicationContext& app)
+{
+
+}
+
+/**
+ * @brief iscore_addon_tutorial::make_commands
+ * This function provides the list of commands available with this
+ * plug-in.
+ */
+std::pair<const CommandParentFactoryKey, CommandGeneratorMap>
+iscore_addon_tutorial::make_commands()
+{
+    return {};
+}
---
+++ iscore_addon_tutorial.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <iscore/plugins/qt_interfaces/PluginRequirements_QtInterface.hpp>
+#include <iscore/plugins/qt_interfaces/FactoryInterface_QtInterface.hpp>
+#include <iscore/plugins/qt_interfaces/FactoryFamily_QtInterface.hpp>
+#include <iscore/plugins/qt_interfaces/GUIApplicationContextPlugin_QtInterface.hpp>
+#include <iscore/plugins/qt_interfaces/CommandFactory_QtInterface.hpp>
+
+#include <iscore/application/ApplicationContext.hpp>
+#include <iscore/command/CommandGeneratorMap.hpp>
+#include <iscore/command/SerializableCommand.hpp>
+#include <iscore/plugins/customfactory/FactoryFamily.hpp>
+
+#include <QObject>
+#include <utility>
+#include <vector>
+
+class iscore_addon_tutorial final :
+        public QObject,
+        public iscore::Plugin_QtInterface,
+        public iscore::FactoryInterface_QtInterface,
+        public iscore::FactoryList_QtInterface,
+        public iscore::GUIApplicationContextPlugin_QtInterface,
+        public iscore::CommandFactory_QtInterface
+{
+        Q_OBJECT
+        Q_PLUGIN_METADATA(IID FactoryInterface_QtInterface_iid)
+        Q_INTERFACES(
+                iscore::Plugin_QtInterface
+                iscore::FactoryInterface_QtInterface
+                iscore::FactoryList_QtInterface
+                iscore::GUIApplicationContextPlugin_QtInterface
+                iscore::CommandFactory_QtInterface
+                )
+
+    public:
+        iscore_addon_tutorial();
+        virtual ~iscore_addon_tutorial();
+
+    private:
+        // Defined in Plugin_QtInterface
+        UuidKey<iscore::Plugin> key() const override;
+        iscore::Version version() const override;
+
+        QStringList required() const override;
+        QStringList offered() const override;
+
+        void updateSaveFile(
+                QJsonObject& obj,
+                iscore::Version obj_version,
+                iscore::Version current_version);
+
+        // Defined in FactoryList_QtInterface
+        std::vector<std::unique_ptr<iscore::FactoryListInterface>> factoryFamilies() override;
+
+        // Defined in FactoryInterface_QtInterface
+        std::vector<std::unique_ptr<iscore::FactoryInterfaceBase>> factories(
+                const iscore::ApplicationContext& ctx,
+                const iscore::AbstractFactoryKey& factoryName) const override;
+
+        // Defined in GUIApplicationContextPlugin_QtInterface
+        iscore::GUIApplicationContextPlugin* make_applicationPlugin(
+                const iscore::GUIApplicationContext& app) override;
+
+        // Defined in CommandFactory_QtInterface
+        std::pair<const CommandParentFactoryKey, CommandGeneratorMap> make_commands() override;
+
+};
